09d806903d4ff6212d052498268cd79c3a06a4b0
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-09-30 22:59:06 +0700

    [#1 install mongoose]

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -21,5 +22,18 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 server.listen(8080,()=>{
     console.log('runnnnnnnn');
-    
 })
+const MONGO_URL = 'mongodb+srv://hodinhtuankiet:hodinhtuankiet@cluster0.komeixn.mongodb.net/?retryWrites=true&w=majority';
+mongoose.Promise = Promise;
+
+mongoose.connection.on('error', (error: Error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+mongoose.connection.on('open', () => {
+  console.log('MongoDB connected successfully!');
+  // Bất kỳ mã xử lý nào bạn muốn thực hiện sau khi kết nối thành công cũng có thể được đặt ở đây.
+});
+
+mongoose.connect(MONGO_URL)
+
